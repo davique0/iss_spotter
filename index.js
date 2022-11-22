55097ce5f6ee88f1dc3a1c93b2a538e8ff2c19ec
davique0 <davique@gmail.com>
2022-11-21 20:55:49 -0500

    handle error for fetch coordinates by IP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +7,13 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP("76.67.105.66", (error, coord)=> {
+  if (error) {
+    console.log("Something is wrong, try again! \n" , error);
+    return;
+  }
+
+  console.log('Returned Coordinates:' , coord);
 });
--- iss.js
+++ iss.js
@@ -16,14 +16,34 @@ const fetchMyIP = (callback) => {
     //even if there is not an initial error in the communications, if the response code is anything but 200 it is consider an error
     if(response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-    callback(Error(msg), null);
-    return;
+      callback(Error(msg), null);
+      return;
     }
     const ipAddress = JSON.parse(body);
       //if everything is fine error us null adn enter our IP as second argument
       callback(null, ipAddress.ip);
   });
-
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = (ip, callback) => {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    const coord = JSON.parse(body);
+    // console.log(coord.success)
+
+    //error handler when success = false
+    if (!coord.success) {
+      const msg = `Success response: ${coord.success} when reading IP: ${coord.ip}, Server says: ${coord.message}`;
+      callback(msg, null);
+      return;
+    };
+
+    // console.log(coord.success)
+
+
+    callback(null, {latitude: coord.latitude, longitude: coord.longitude})
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
